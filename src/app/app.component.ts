61eb644d257e21f39956e3994d3193f0afc063f1
Sai <78162001+sai95ui@users.noreply.github.com>
2021-01-28 23:50:31 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService, SearchParams } from '../app/services/api-service.service';
+import {QuestionnaireComponentComponent} from './questionnaire-component/questionnaire-component.component';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
-
+import {QuestionnaireComponentComponent} from './questionnaire-component/questionnaire-component.component';
 import { AppComponent } from './app.component';
 import { ApiService } from './services/api-service.service';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionnaireComponentComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    RouterModule
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'fetch', pathMatch: 'full' },
+      {
+         path: 'quetions',
+         component: QuestionnaireComponentComponent
+      },
+      {
+        path: 'fetch',
+        component: AppComponent
+     }
+   ])
   ],
   providers: [
     ApiService
